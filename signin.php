b9b63be18192bb936a6dd00a5ffaf066e023e019
Konstantin Schmidt <31983699+Assimilationstheorie@users.noreply.github.com>
2018-01-19 20:34:48 +0100

    first step

--- signin.php
+++ signin.php
@@ -1,14 +1,21 @@
 <?php
+require_once('Configuration/databaseConnection.php');
 
-include 'Configuration/databaseConnection.php';
-
+//No WAY :D
 $name = $password = "";
 
 if ($_POST['name'] == "Zsolt")
 {
-	if ($_POST['password'] == "123")
+	if ($_POST['password'] == 123)
 	{
 		header('Location: index.php');
 	}
+}
+
+//Maybe you try this.
+$name = $_POST['name'];
+$password = $_POST['password'];
+if($name == "Zsolt" && $password == 123) {
+ header('Location: index.php');	
 }
 ?>
